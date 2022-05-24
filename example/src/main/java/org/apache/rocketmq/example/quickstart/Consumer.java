a419e67813b2aba50110bbb19af39de4a484c61d
yangyuru <yiming2008@126.com>
2022-05-24 23:57:14 +0800

    feat: add conf dir for version 4.6

--- example/src/main/java/org/apache/rocketmq/example/quickstart/Consumer.java
+++ example/src/main/java/org/apache/rocketmq/example/quickstart/Consumer.java
@@ -35,7 +35,8 @@ public class Consumer {
         /*
          * Instantiate with specified consumer group name.
          */
-        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("please_rename_unique_group_name_4");
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("please_rename_unique_group_name");
+        consumer.setNamesrvAddr("localhost:9876");
 
         /*
          * Specify name server addresses.
